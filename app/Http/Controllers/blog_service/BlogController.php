0a53161e59c5a93338f610c2585ee6249209478a
Hermance2 <hermancerandria@gmail.com>
2024-11-20 19:08:13 +0300

    create table blog

---
+++ app/Http/Controllers/blog_service/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\blog_service;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\blog_service\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/blog')->controller(BlogController::class)->name('blog.')->group(function(){
+    Route::get('/','index')->name('list');//list blog
+    Route::get('/create','create')->name('.create');
 });
